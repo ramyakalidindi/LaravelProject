0abb2df099da791c8c14ec9e9c5450bb19e21c4d
ramya <ramya.mounami@gmail.com>
2018-11-02 21:33:05 -0400

    Unit Test1: To insert a user into the users table

---
+++ tests/Feature/InsertingUserTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\User;
+
+class InsertingUserTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testInsertingUser()
+    {
+
+        $user = factory(\App\User::class)->create();
+        $this->assertDatabaseHas('users', ['id' => $user->id]);
+    }
+}
